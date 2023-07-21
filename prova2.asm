9f79a062cd711871f8ad64e0a4cc4e9b5c03aaa6
arthur brito <arthurmedeiros32@gmail.com>
2023-07-21 12:07:35 -0300

    feat: add prova2 from monitor

--- prova2.asm
+++ prova2.asm
@@ -4,46 +4,60 @@ org 0x7C00
 jmp main
 
 main:
-
-  get_n1:
-
-  mov ah, 0x00
-  int 16h
-  mov ah, 0
-
-  sub al, '0'
-  
-  mov bx, ax ; Armazena o primeiro número em bx
-
-  get_n2:
+  get_x:
 
   mov ah, 0x00
   int 16h
-  mov ah, 0
 
+  mov ah, 0
   sub al, '0'
 
-  mov ax, bx ; Move o primeiro número para ax
-  xor dx, dx ; Limpa o dx para a próxima divisão
+  push ax
 
-  div ax      ; Divide o segundo número pelo primeiro
+  get_y:
 
-  mov ax, dx  ; Move o resto para ax
+  mov ah, 0x00
+  int 16h
 
-  cmp ax, 0
-  je done     ; Se o resto é zero, bx é o MDC
-  
-  mov bx, ax  ; Move o resto para bx
-  jmp get_n2  ; Começa o loop novamente
+  mov ah, 0
+  sub al, '0'
 
-done:
+  push ax
 
-  push bx
+  call mdc
+
+  push ax
   call print_number
 
   jmp end
 
-print_number:
+mdc:               ; Vamos considerar o retorno dessa rotina pelo ax
+  push bp
+  mov bp, sp          ; Criando um novo "contexto" na pilha para essa chamada de mdc
+
+  mov ax, [bp+6]      ; ax = x
+  div byte[bp+4]      ; ah = x % y, al = x / y
+  or ah, ah           ; cmp ah, 0
+  jnz out1            ; jneq out1
+
+  out0:              ; Saída se n == 0
+  mov ax, [bp+4]      ; Retorno = y
+  pop bp
+  ret 4               ; Lembrar de remover o parâmetro da pilha
+
+  out1:              ; Saída se n != 0
+                      ; Passando os parâmetros para a chamada de mdc(y, x%y)
+  push word[bp+4]     ; push y
+  mov al, ah          ; al = x%y
+  mov ah, 0           ; ah = 0 -> ax = x%y
+  push ax             ; push x%y
+
+  call mdc            ; Recursão de mdc(y, x%y) que retorna o resultado em ax, que é retornado para a chamada anterior
+  pop bp
+  ret 4               ; Lembrar de remover o parâmetro da pilha
+
+
+print_number:      ; Print number pra testar
     push bp
     mov bp, sp
     mov ax, [bp+4]    
